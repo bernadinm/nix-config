a9f32a37ee0883b5a0268db2d6baed74eaa37983
Miguel Bernadin <miguel.bernadin@gmail.com>
2018-10-03 16:43:41 -0700

    nix: adding bc and going back to vimHugeX

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,7 @@
 {
   imports = [
   <nixpkgs/nixos/modules/installer/virtualbox-demo.nix>
-  "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos"
+  #"${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos"
   ];
   services.openssh.enable = true;
 
@@ -14,14 +14,13 @@
   #networking.firewall.allowedTCPPorts = [ 22 ];
 
   environment.systemPackages = with pkgs; [
-   vim
+   vimHugeX
    mkpasswd
    (vim_configurable.customize {
-      name = "vim";
+      name = "vimHugeX";
       vimrcConfig.customRC = ''
         syntax on
         set mouse-=a
-        set number
       '';
     })
   ];
@@ -46,6 +45,7 @@
       pkgs.curl
       pkgs.traceroute
       pkgs.tree
+      pkgs.bc
     ];
     openssh.authorizedKeys.keys =
   [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCziuN8QzK0XAvmXVJ2glkVJuGySd2KwGBrQlCP4UFyGOWeh5FEXgL7/kdfx0TZKLMEXrDs8al1Ve7tL9DBnTWpVAimqjj3T+5G2S8Z/KG2eoG+ADd26KkjJOh3MhVBk6CLY3vTJzIK2jr7ljn/fZ01NS/KOTYvmzqQo6r/cIYt9NoNxS9KfzGDrAR+IA+QhbI2SojzVt6GcmJpKmKwtevANIhL/qCZ1x+Z+hZ1cPW2EwVazkpeGluMXv08zVkoivXjkxks3Pjj11EyuBD1UmuRkyE7ve24U0Cvk+jgjP2aA6ElTNf/p20QA1Wpi/84vICoAsIVt7F28n4j/4u2TgeD mbernadin@mesospheres-MacBook-Pro-2.local" ];
