f3a1519fa79457cb46be77a1e66f6512ba40410f
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2019-02-04 20:35:08 +0000

    nix: updated proper ssh-agent socket config

--- configuration.nix
+++ configuration.nix
@@ -70,7 +70,14 @@
    if ! [[ ( -f ~/.gitconfig ) ]]; then ln -s ~/g/.gitconfig ~/.gitconfig; fi
    
    # add ssh keys
-   ps -ef | grep ssh-agent | grep mb | tail -1 | while read x; do if [[ "$x" = "" ]]; then eval $(ssh-agent -s); fi; done
+   if [[ "$(ps -ef | grep ssh-agent | grep mb | grep -v grep | tail -1)" = "" ]]; then
+     eval $(ssh-agent -s -a ~/.ssh-agent-socket);
+   else
+     SSH_AGENT_PID="$(ps -ef | grep ssh-agent | grep mb | awk '{print $2}' | head -1)";
+     SSH_AUTH_SOCK="$HOME/.ssh-agent-socket";
+     export SSH_AGENT_PID="$(ps -ef | grep ssh-agent | grep mb | grep -v grep | awk '{print $2}' | head -1)";
+     export SSH_AUTH_SOCK="$HOME/.ssh-agent-socket";
+   fi
    grep -slR "PRIVATE" ~/.ssh/ | xargs ssh-add
 
    export GPG_TTY="$(tty)" #TODO(bernadinm): https://github.com/keybase/keybase-issues/issues/2798
