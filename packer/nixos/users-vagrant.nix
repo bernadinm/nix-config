18e3f4759f1f5448d86cc8fe8bebce1d888539b5
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2019-04-30 11:02:39 -0700

    nix: adding retry when connection dies to avoid reboot
    Related to https://stackoverflow.com/questions/24966676/transport-endpoint-is-not-connected . A few options here is to downgrade to 0.1.38 or use one where the fix resides

--- packer/nixos/users-vagrant.nix
+++ packer/nixos/users-vagrant.nix
@@ -79,7 +79,7 @@
    # Persist NixOS on GCP
    # Prereq: gsutil mb -c regional -l us-west1 gs://nixos-persist
    mkdir -p ~/g
-   if ! pgrep -x gcsfuse &>/dev/null; then gcsfuse -file-mode=600 nixos-persist ~/g; fi 
+   if ! pgrep -x gcsfuse &>/dev/null; then gcsfuse -file-mode=600 nixos-persist ~/g || fusermount -uz ~/g && gcsfuse -file-mode=600 nixos-persist ~/g; fi
    
    # Setting vi alias to vim
    alias vi="vim"
