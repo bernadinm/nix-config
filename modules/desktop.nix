a11681e0a5c597b863daef73e4e7425f87213a6d
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2021-11-21 18:51:48 -0800

    lumina: splitting up to components

---
+++ modules/desktop.nix
@@ -0,0 +1,89 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    # base
+    google-chrome
+    xclip
+    xsel
+    x2goclient
+    torbrowser
+    chromium
+
+    # Plasma desktop
+    kdeplasma-addons
+    kdeconnect
+    kdenlive
+    okular
+    konversation
+    fusuma
+    gwenview
+
+    # Fonts
+    font-awesome
+    compton
+    lxqt.compton-conf
+
+    feh # wallpaper manager
+    vifm ranger nnn # text file manager
+    rofi
+    dmenu
+
+    w3m-full
+ 
+    spectacle # screenshot capture util
+    kile # latex authoring tool for kde
+    (texlive.combine {
+      inherit (texlive) scheme-small titling collection-langfrench cm-super;
+    })
+
+  ];
+
+  services.xserver = {
+    enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+    displayManager = {
+      defaultSession = "none+i3";
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        rofi
+        polybar
+        clipit
+        xorg.xprop
+        xautolock # timer to lock screen
+        i3status # gives you the default i3 status bar
+        i3lock-fancy-rapid #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
+
+        compton
+        lxqt.compton-conf
+
+        feh # wallpaper manager
+        vifm # graphic file manager
+      ];
+    };
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.miguel = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" "video" "i2c" ]; # Enable ‘sudo’ for the user.
+    description = "Miguel Bernadin";
+  };
+
+  users.users.rachelle = {
+    isNormalUser = true;
+    extraGroups = [ ]; # Enable ‘sudo’ for the user.
+    description = "Rachelle Bernadin";
+  };
+
+
+}
---
+++ modules/utilities.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    # base
+     wget vim unzip
+     python3 python37Packages.virtualenv python37Packages.pip gcc libffi
+     python37Packages.pillow
+     python37Packages.setuptools
+     git
+     bash
+     glib
+     ddgr
+     terraform
+     fast-cli
+     pcmanfm
+     gspeech
+     lynx
+     whois
+     trash-cli
+     gnumake
+     mimic
+     picotts
+     bind
+     ag
+     ripgrep
+     aria
+     killall
+     bc
+     gh
+     termite
+     neofetch
+
+     #(import (builtins.fetchTarball https://github.com/hercules-ci/arion/tarball/master) {}).arion
+
+     navi fzf
+     go
+     nnn
+     nixpkgs-fmt
+
+     # ML
+     gpt2tc
+
+  ];
+}
