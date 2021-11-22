673a22e8bf90fa719995ab74ef3f4bb1e58f2a03
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2021-11-21 18:58:48 -0800

    nixpkgs-fmt everything

--- modules/utilities.nix
+++ modules/utilities.nix
@@ -3,43 +3,49 @@
 {
   environment.systemPackages = with pkgs; [
     # base
-     wget vim unzip
-     python3 python37Packages.virtualenv python37Packages.pip gcc libffi
-     python37Packages.pillow
-     python37Packages.setuptools
-     git
-     bash
-     glib
-     ddgr
-     terraform
-     fast-cli
-     pcmanfm
-     gspeech
-     lynx
-     whois
-     trash-cli
-     gnumake
-     mimic
-     picotts
-     bind
-     ag
-     ripgrep
-     aria
-     killall
-     bc
-     gh
-     termite
-     neofetch
+    wget
+    vim
+    unzip
+    python3
+    python37Packages.virtualenv
+    python37Packages.pip
+    gcc
+    libffi
+    python37Packages.pillow
+    python37Packages.setuptools
+    git
+    bash
+    glib
+    ddgr
+    terraform
+    fast-cli
+    pcmanfm
+    gspeech
+    lynx
+    whois
+    trash-cli
+    gnumake
+    mimic
+    picotts
+    bind
+    ag
+    ripgrep
+    aria
+    killall
+    bc
+    gh
+    termite
+    neofetch
 
-     #(import (builtins.fetchTarball https://github.com/hercules-ci/arion/tarball/master) {}).arion
+    #(import (builtins.fetchTarball https://github.com/hercules-ci/arion/tarball/master) {}).arion
 
-     navi fzf
-     go
-     nnn
-     nixpkgs-fmt
+    navi
+    fzf
+    go
+    nixpkgs-fmt
 
-     # ML
-     gpt2tc
+    # ML
+    gpt2tc
 
   ];
 }
