3745cd094066d78f5c30c811bd7a5c7699443580
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2022-05-25 18:39:23 -0700

    util: adding nvim

--- modules/utilities.nix
+++ modules/utilities.nix
@@ -5,7 +5,25 @@
     # base
     wget # system
     vim # system
-    neovim # system
+    neovim
+    #(neovim.override {
+    #  vimAlias = true;
+    #  configure = {
+    #    customRC = builtins.readFile ./config/init.vim;
+    #    packages.myPlugins = with pkgs.vimPlugins; {
+    #    start = [
+    #      vim-surround # Shortcuts for setting () {} etc.
+    #      coc-nvim coc-git coc-highlight coc-python coc-rls coc-vetur coc-vimtex coc-yaml coc-html coc-json # auto completion
+    #      vim-nix # nix highlight
+    #      vimtex # latex stuff
+    #      fzf-vim # fuzzy finder through vim
+    #      nerdtree # file structure inside nvim
+    #      rainbow # Color parenthesis
+    #    ];
+    #    opt = [];
+    #    };
+    #  };
+    #})
     tmux # system
     unzip # system
     rar # archives
