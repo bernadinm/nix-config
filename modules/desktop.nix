21d2a1783f83b5376b646336a3f5ab93753e32c9
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2022-07-10 14:37:37 -0700

    desktop: adding coc settings

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -153,6 +153,8 @@ in
     url = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
     sha256 = "sha256-4tvXyNcyrnl+UFnA3B6WS5RSmjLQfQUdXQWHJ0YqQ/0=";
   };
+  home-manager.users.miguel.home.file.".config/nvim/coc-settings.json".source =
+    ../dotfiles/vim/coc-settings.json;
   home-manager.users.miguel.home.stateVersion = "22.05";
   home-manager.users.miguel.programs.neovim = {
     enable = true;
