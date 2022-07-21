2653a8f43b1f33f3b7dfe0fd48b5c868c3a600b3
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2022-07-21 08:45:20 -0700

    desktop: set hm from master to 22.05 version

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -2,7 +2,7 @@
 
 let
   home-manager = builtins.fetchTarball
- "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+ "https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz";
 in
 {
   imports =
