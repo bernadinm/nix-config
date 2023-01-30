d64dfff8d799a660a3d754e593c1d6a6aad5720c
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2023-01-29 22:01:17 -0800

    test: testing flake with dream2nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,30 @@
 {
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.Luna = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./hosts/luna/configuration.nix ];
-    };
+  inputs = {
+    dream2nix.url = "github:nix-community/dream2nix";
+    src.url = "https://registry.npmjs.org/eslint/-/eslint-8.4.1.tgz";
+    src.flake = false;
   };
+
+  outputs = {
+    self,
+    dream2nix,
+    src,
+  } @ inp:
+    (dream2nix.lib.makeFlakeOutputs {
+      systems = ["x86_64-linux"];
+      config.projectRoot = ./.;
+      source = src;
+      projects = ./projects.toml;
+    })
+    // {
+      # checks = self.packages;
+    };
+  
+  #outputs = { self, nixpkgs, ... }@attrs: {
+  #  nixosConfigurations.Luna = nixpkgs.lib.nixosSystem {
+  #    specialArgs = attrs;
+  #    system = "x86_64-linux";
+  #    modules = [ ./hosts/luna/configuration.nix ];
+  #  };
+  #};
 }
