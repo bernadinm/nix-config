fef00d463b5b3dfc6e456455c83ca3773da5250f
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2023-03-04 20:29:12 -0800

    wip: adding npm pkgs

--- modules/utilities.nix
+++ modules/utilities.nix
@@ -6,6 +6,11 @@ let
   unstable = import <nixos-unstable> { config = baseconfig; };
 in
 {
+  imports =
+    [
+      ../pkgs/npm.nix
+    ];
+
   environment.systemPackages = with pkgs; [
     # base
     wget # system
