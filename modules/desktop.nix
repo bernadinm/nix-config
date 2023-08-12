d68b3e1d110ea95d5a17ceedb9c2dbec182a42b0
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2023-08-12 16:26:09 -0700

    fix: addressing a bug in helix helixe::index::slice_end_index_len_fail errors

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 let
+  baseconfig = { allowUnfree = true; };
+  unstable = import <nixos-unstable> { config = baseconfig; };
   home-manager = builtins.fetchTarball
     "https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz";
 in
