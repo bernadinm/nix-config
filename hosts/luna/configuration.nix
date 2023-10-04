439bc43b7144131dbb73b45f76a0df0a4e3a3e02
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2023-10-03 23:17:34 -0700

    fix: removing nixos framework hardware

--- hosts/luna/configuration.nix
+++ hosts/luna/configuration.nix
@@ -12,7 +12,6 @@ in
   imports =
     [
       # Include the results of the hardware scan.
-      <nixos-hardware/framework>
       ./hardware-configuration.nix
       ../../modules/gdrivesync.nix
       ../../modules/entertainment.nix
