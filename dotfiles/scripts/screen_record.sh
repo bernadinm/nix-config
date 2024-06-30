44eb8f9b4192705967d3caa98238603541a2659f
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2024-06-30 09:58:46 -0700

    feat: update SSR script

--- dotfiles/scripts/screen_record.sh
+++ dotfiles/scripts/screen_record.sh
@@ -1,13 +1,45 @@
 #!/run/current-system/sw/bin/bash
 
-# Check if SimpleScreenRecorder is running
-if pgrep -f 'simplescreenrecorder --start-hidden --start-recording' > /dev/null
-then
-    # If it is running, send the stop signal and show a notification
-    pkill -f 'simplescreenrecorder --start-hidden --start-recording'
-    notify-send "SimpleScreenRecorder" "Recording stopped."
+# TODO(bernadinm): fix this, it stays running and doesn't properly end some times
+set -x
+STATE_FILE="/tmp/ssr_state"
+COMMAND_PIPE="/tmp/ssr_command_pipe"
+
+# Ensure the command pipe exists
+if [ ! -p "$COMMAND_PIPE" ]; then
+    mkfifo "$COMMAND_PIPE"
+fi
+
+# Function to ensure SSR is running and set up to read from the command pipe
+ensure_ssr_running() {
+    if ! pgrep -x "simplescreenrecorder" > /dev/null; then
+        # Start SimpleScreenRecorder and ensure it reads from the command pipe
+        simplescreenrecorder --start-hidden < "$COMMAND_PIPE" &
+        echo "SimpleScreenRecorder started."
+        notify-send -t 5000 "SimpleScreenRecorder (SSR)" "SSR was not running, started now."
+    fi
+}
+
+# Function to start recording
+start_recording() {
+    echo "record-start" > "$COMMAND_PIPE"
+    echo "recording" > "$STATE_FILE"
+    notify-send -t 1000 "SimpleScreenRecorder (SSR)" "Recording started."
+}
+
+# Function to stop recording
+stop_recording() {
+    echo "record-save" > "$COMMAND_PIPE"
+    rm "$STATE_FILE"
+    notify-send -t 5000 "SimpleScreenRecorder (SSR)" "Recording saved."
+}
+
+# Ensure SSR is running
+ensure_ssr_running
+
+# Toggle recording based on current state
+if [ -f "$STATE_FILE" ]; then
+    stop_recording
 else
-    # If it is not running, start it with the specified settings and show a notification
-    notify-send -t 1000 "SimpleScreenRecorder" "Recording started."
-    simplescreenrecorder --start-hidden --start-recording 
+    start_recording
 fi
