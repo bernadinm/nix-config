612f8d7f64aa5365fb94ef18c743287abee0de4a
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2026-06-29 02:00:08 +0300

    fix(packer): deprecate non-flake nixos-rebuild commands

--- packer/scripts/postinstall.sh
+++ packer/scripts/postinstall.sh
@@ -2,11 +2,16 @@
 
 set -e
 
+# DEPRECATED: This script is for legacy packer/vagrant builds only.
+# For actual system rebuilds, always use flakes:
+#   sudo nixos-rebuild switch --flake /home/miguel/git/bernadinm/nix-config#<hostname>
+
 # Switch to non ssl channel as there are issues with certs
 # TODO: fix this propperly by including the root certs.
 nix-channel --remove nixos
 nix-channel --add http://nixos.org/channels/nixos-unstable nixos
-nixos-rebuild switch --upgrade
+# LEGACY: nixos-rebuild switch --upgrade
+echo "WARNING: Use flake-based rebuild instead"
 
 # Cleanup any previous generations and delete old packa
 nix-collect-garbage -d
