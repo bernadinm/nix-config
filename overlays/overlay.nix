006a750584548eb59a1beaea1a9c37e52441dc48
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2026-07-07 21:11:33 +0300

    fix(waydroid): use iptables-nft instead of iptables-legacy

--- overlays/overlay.nix
+++ overlays/overlay.nix
@@ -2,5 +2,15 @@ self: super:
 
 {
   frappe-books = super.callPackage ../local-packages/frappe-books/frappe-books.nix { };
+
+  # Fix Waydroid to use nftables instead of iptables-legacy (kernel 6.x doesn't have ip_tables module)
+  waydroid = super.waydroid.overrideAttrs (oldAttrs: {
+    postPatch = (oldAttrs.postPatch or "") + ''
+      # Force use of iptables-nft instead of iptables-legacy
+      substituteInPlace data/scripts/waydroid-net.sh \
+        --replace 'IPTABLES_BIN="$(command -v iptables-legacy)"' 'IPTABLES_BIN=""' \
+        --replace 'IP6TABLES_BIN="$(command -v ip6tables-legacy)"' 'IP6TABLES_BIN=""'
+    '';
+  });
 }
 
